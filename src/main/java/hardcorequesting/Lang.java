24a294f8f5ffcf78c681dc6b88cb86fab4c8dbc9
インク <168640573+Xiaomo305Mua@users.noreply.github.com>
2026-09-07 12:53:34 +0800

    更新 Lang.java

--- src/main/java/hardcorequesting/Lang.java
+++ src/main/java/hardcorequesting/Lang.java
@@ -5,6 +5,9 @@ public class Lang {
     public static final String PREFIX = "hqm.";
 
 
+    // ======================================
+    //				COMMAND
+    // ======================================
     public static final String COMMAND_PREFIX = PREFIX + "command.";
 
     public static final String NOT_FOUND = COMMAND_PREFIX + "notFound";
@@ -15,21 +18,12 @@ public class Lang {
     public static final String LOAD_SUCCESS = COMMAND_PREFIX + "loadSuccess";
     public static final String FILE_NOT_FOUND = COMMAND_PREFIX + "fileNotFound";
     public static final String QUEST_NOT_FOUND = COMMAND_PREFIX + "questNotFound";
+    public static final String CONVERT_USAGE = COMMAND_PREFIX + "convertUsage";
+    public static final String CONVERT_UNSUPPORTED = COMMAND_PREFIX + "convertUnsupported";
+    public static final String CONVERT_SUCCESS = COMMAND_PREFIX + "convertSuccess";
+    public static final String CONVERT_ERROR = COMMAND_PREFIX + "convertError";
     public static final String HELP_START = COMMAND_PREFIX + "info.help.start";
 
     public static final String INFO_SUFFIX = ".info";
     public static final String SYNTAX_SUFFIX = ".syntax";
-
-    public static final String CONVERT_USAGE = COMMAND_PREFIX + "convert.usage";
-    public static final String CONVERT_SUCCESS = COMMAND_PREFIX + "convert.success";
-    public static final String CONVERT_ERROR = COMMAND_PREFIX + "convert.error";
-    public static final String CONVERT_UNSUPPORTED = COMMAND_PREFIX + "convert.unsupported";
-    public static final String SAVE_SUCCESS_NAME = COMMAND_PREFIX + "save.success.name";
-    public static final String SAVE_SUCCESS_TEXT = COMMAND_PREFIX + "save.success.text";
-    public static final String BACKUP_FAIL_NAME = COMMAND_PREFIX + "save.backupFail.name";
-    public static final String BACKUP_FAIL_TEXT = COMMAND_PREFIX + "save.backupFail.text";
-    public static final String SAVE_FAIL_NAME = COMMAND_PREFIX + "save.saveFail.name";
-    public static final String SAVE_FAIL_TEXT = COMMAND_PREFIX + "save.saveFail.text";
-    public static final String PRE_CRASH_FAILURE_NAME = COMMAND_PREFIX + "save.preCrashFail.name";
-    public static final String PRE_CRASH_FAILURE_TEXT = COMMAND_PREFIX + "save.preCrashFail.text";
 }
